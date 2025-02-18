a3d624d3bfd76560ccdc858201119adc85e85b27
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2025-02-18 20:36:11 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -40,9 +40,10 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -53,28 +54,30 @@ def test_update_user(client, user):
     assert response.json() == {
         'username': 'bob',
         'email': 'bob@example.com',
-        'id': 1,
+        'id': user.id,
     }
 
-def test_update_integrity_error(client, user):
+
+def test_update_integrity_error(client, user, token):
     # Criando um registro para "fausto"
     client.post(
-        '/users',
+        '/users/',
         json={
             'username': 'fausto',
             'email': 'fausto@exemple.com',
             'password': 'secret',
-        }
+        },
     )
 
     # Alterando o user.username das fixture para fausto
     response_update = client.put(
         f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'fausto',
             'email': 'bob@exemplo.com',
             'password': 'mynewpassword',
-        }
+        },
     )
     assert response_update.status_code == HTTPStatus.CONFLICT
     assert response_update.json() == {
@@ -82,8 +85,23 @@ def test_update_integrity_error(client, user):
     }
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
